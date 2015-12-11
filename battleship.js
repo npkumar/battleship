fd0744f2782002a7e37a5556aca33b907e3749c4
Nitin Pradeep Kumar <nkumar@appcelerator.com>
2015-12-11 12:06:13 +0800

    call fire model if valid guess

--- battleship.js
+++ battleship.js
@@ -61,11 +61,14 @@ var controller = {
     processGuess: function(guess) {
         var location = parseGuess(guess);
         if (location){
-            
+            this.guesses++;
+            var hit = model.fire(location);
+            if (hit && model.shipsSunk === model.numShips) { 
+                view.displayMessage("You sank all my battleships, in " + this.guesses + " guesses");
+            }
         }
     }
 };
-
 
 function parseGuess(guess) {
     var alphabet = ["A", "B", "C", "D", "E", "F", "G"];
@@ -87,4 +90,3 @@ function parseGuess(guess) {
     }
     return null;
 };
-    ￼￼
