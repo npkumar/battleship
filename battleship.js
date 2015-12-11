ecde30a12f005dafd1738c10c2ad045741e5b5e9
Nitin Pradeep Kumar <nkumar@appcelerator.com>
2015-12-11 11:58:44 +0800

    parse guess logic

--- battleship.js
+++ battleship.js
@@ -54,3 +54,37 @@ var model = {
         return true; 
     }
 };
+
+
+var controller = { 
+    guesses: 0,
+    processGuess: function(guess) {
+        var location = parseGuess(guess);
+        if (location){
+            
+        }
+    }
+};
+
+
+function parseGuess(guess) {
+    var alphabet = ["A", "B", "C", "D", "E", "F", "G"];
+    
+    if (guess === null || guess.length !== 2) {
+        alert("Oops, please enter a letter and a number on the board."); 
+    } else {
+      var firstChar = guess.charAt(0);
+      var row = alphabet.indexOf(firstChar);
+      var column = guess.charAt(1);
+      
+      if (isNaN(row) || isNaN(column)) { 
+          alert("Oops, that isn't on the board.");
+      } else if (row < 0 || row >= model.boardSize || column < 0 || column >= model.boardSize) {
+          alert("Oops, that's off the board!"); 
+      } else {
+          return row + column;
+      }
+    }
+    return null;
+};
+    ￼￼
